f57b27545e2bf93d880d2c157acca184fc94e1fa
leohsieh0907-del <leohsieh0907@gmail.com>
2026-07-13 00:13:22 +0800

    v16: 首頁加版本與更新日期標記

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'mathgo-v4';
+var CACHE = 'mathgo-v5';
 var CORE = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png', 'apple-touch-icon.png'];
 
 self.addEventListener('install', function (e) {
