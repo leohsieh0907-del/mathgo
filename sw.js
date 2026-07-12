e7fd3a3310d2300016ac72ae8fecca6a5f555f39
leohsieh0907-del <leohsieh0907@gmail.com>
2026-07-12 22:02:09 +0800

    deploy v16: 高一其餘4章(數與式/直線與圓/多項式函數/指對數)仿歷屆+三型adv-sol詳解

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'mathgo-v3';
+var CACHE = 'mathgo-v4';
 var CORE = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png', 'apple-touch-icon.png'];
 
 self.addEventListener('install', function (e) {
